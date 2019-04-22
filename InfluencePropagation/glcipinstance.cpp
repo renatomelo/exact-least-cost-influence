b74c76dbdd73c6551870e82c02e8b648c1ce06a2
rsmelo@inf.ufpr.br <rsmelo@inf.ufpr.br>
2019-04-22 20:25:35 -0300

    first instance

--- InfluencePropagation/glcipinstance.cpp
+++ InfluencePropagation/glcipinstance.cpp
@@ -1,7 +1,7 @@
 #include "glcipinstance.h"
 
 GLCIPInstance::GLCIPInstance(Digraph &g, DNodePosMap &posx, DNodePosMap &posy, ArcValueMap &influence, DNodeValueMap &threshold,
-    DNodeListOfValuesMap &incentives, double alpha, int n, int m) : g(g), influence(influence), threshold(threshold),
+    DNodeValueVectorMap &incentives, double alpha, int n, int m) : g(g), influence(influence), threshold(threshold),
     incentives(incentives), posx(posx), posy(posy)
 {
     this->alpha = alpha;
